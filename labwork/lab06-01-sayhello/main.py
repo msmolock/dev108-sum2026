44948e20e8c8b8a923802fb191a215b03fe52b33
Michael Smolock <156984408+msmolock@users.noreply.github.com>
2026-07-29 04:52:57 +0000

    Lab 6 Part 1: nameformat module and main.py complete

--- labwork/lab06-01-sayhello/main.py
+++ labwork/lab06-01-sayhello/main.py
@@ -1,1 +1,61 @@
-# starting file for lab 6
+# Michael Smolock
+# 7/27/2026
+# Dev 108
+# Lab6-1
+#!/usr/bin/env python3
+
+import nameformat
+
+def main():
+    print("The NameFront Module")
+    print()
+    print("Hello! This will call and test a module's function.")
+
+# First name and last name input
+    first_name = input("Please enter your first name: ")
+    last_name = input("Please enter your last name: ")
+
+# Menu loop
+    while True:
+        print("\n** MENU **") 
+        print("1 - Say Hello")
+        print("2 - Output full name")
+        print("3 - Output last name, first name")
+        print("4 - Read documentation")
+        print("5 - Exit")
+
+# Choices
+        choice = input("Please choose ")
+        print()
+
+        if choice == "1":
+            greeting = nameformat.sayHello(first_name)
+            print(greeting)
+
+        elif choice == "2":
+            full = nameformat.fullName(first_name, last_name)
+            print(full)
+
+        elif choice == "3":
+            reversed_name = nameformat.lastNameFirst(first_name, last_name )
+            print(reversed_name)
+
+        elif choice == "4":
+            print("== Module Documentation ==")
+            print("(Press 'q' to exit the documentation viewer)")
+            help(nameformat)
+
+        elif choice == "5":
+            print("Goodbye")
+            break
+
+        else:
+            print("Invalid selection. Please choose an option from 1 to 5")
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+        
--- labwork/lab06-01-sayhello/nameformat.py
+++ labwork/lab06-01-sayhello/nameformat.py
@@ -1,12 +1,24 @@
-# This is where you will code your three functions 
-# Be sure to write documentation for this module. Refer to your book chapter for instructions on how to do this.
+#!/usr/bin/env python3
+"""
+# Michael Smolock
+# 7/27/2026
+#Dev 108
+# Lab6-1
 
-# sayHello() ex: Hello Tony!
+# sayHello() ex: Hello Mike!
+"""
+    
+def sayHello(firstName):
+    """Takes a first name and returns a greeting string."""
+    return f"Hello {firstName}!"
 
+# fullName() ex: Mike Smolock
+def fullName(firstName, lastName):
+    """Takes a first name and last name and returns them combined as one string."""
+    return f"{firstName} {lastName}"
 
+# lastNameFirst() ex: Smolock, Mike
+def lastNameFirst(firstName, lastName):
+    """Takes a first name and last name and returns them as 'LastName, FirstName.'"""
+    return f"{lastName}, {firstName}"
 
-# fullName() ex: Tony Stark
-
-
-
-# lastNameFirst() ex: Stark, Tony
