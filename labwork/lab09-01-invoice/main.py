9ec5a027f74d395b597fada9fb566aa4dd1980aa
Michael Smolock <156984408+msmolock@users.noreply.github.com>
2026-08-09 01:47:50 +0000

    Lab 9 Exercise 9-1: Invoice program with shipping cost and locale currency formatting

--- labwork/lab09-01-invoice/main.py
+++ labwork/lab09-01-invoice/main.py
@@ -1,4 +1,12 @@
+#!/usr/bin/env python3
+# Michael Smolock
+# August 8, 2026
+# Dev 108
+# Exercise 9-1: Invoice program with shipping cost and locale currency formatting
+
+
 import locale as lc
+
 from decimal import Decimal
 from decimal import ROUND_HALF_UP
 
@@ -27,21 +35,26 @@ while choice == "y":
     discount = discount.quantize(Decimal("1.00"), ROUND_HALF_UP)                                
     subtotal = order_total - discount
 
+    # Calculate shipping cost (8.5% of subtotal)
     shipping_percent = Decimal(".085")
     shipping_cost = subtotal * shipping_percent
     shipping_cost = shipping_cost.quantize(Decimal("1.00"), ROUND_HALF_UP)
 
+    # Calculate sales tax (5% of subtotal)
     tax_percent = Decimal(".05")
     sales_tax = subtotal * tax_percent
     sales_tax = sales_tax.quantize(Decimal("1.00"), ROUND_HALF_UP)                                 
     invoice_total = subtotal + shipping_cost + sales_tax
 
+    # Set locale for currency formatting 
     lc.setlocale(lc.LC_ALL, 'en_US.UTF-8 ')
-    
+
+
+    # Format order total and invoice total with $
     order_total = lc.currency(order_total, grouping=True)
     invoice_total = lc.currency(invoice_total, grouping=True)
 
-    # display the results
+    # display the formatted results
     print(f"Order total:        {order_total:>10}")
     print(f"Discount amount:    {discount:10,}")
     print(f"Subtotal:           {subtotal:10,}")
