2eb3bb4697decfbfaf7cff6fb5e3f347bb8f30f6
Michael Smolock <156984408+msmolock@users.noreply.github.com>
2026-08-09 01:15:04 +0000

    Lab 9 Exercise 9-1: Invoice program with shipping cost and locale currency formatting

--- labwork/lab09-01-invoice/main.py
+++ labwork/lab09-01-invoice/main.py
@@ -1,3 +1,4 @@
+import locale as lc
 from decimal import Decimal
 from decimal import ROUND_HALF_UP
 
@@ -25,17 +26,28 @@ while choice == "y":
     discount = order_total * discount_percent
     discount = discount.quantize(Decimal("1.00"), ROUND_HALF_UP)                                
     subtotal = order_total - discount
+
+    shipping_percent = Decimal(".085")
+    shipping_cost = subtotal * shipping_percent
+    shipping_cost = shipping_cost.quantize(Decimal("1.00"), ROUND_HALF_UP)
+
     tax_percent = Decimal(".05")
     sales_tax = subtotal * tax_percent
     sales_tax = sales_tax.quantize(Decimal("1.00"), ROUND_HALF_UP)                                 
-    invoice_total = subtotal + sales_tax
+    invoice_total = subtotal + shipping_cost + sales_tax
+
+    lc.setlocale(lc.LC_ALL, 'en_US.UTF-8 ')
+    
+    order_total = lc.currency(order_total, grouping=True)
+    invoice_total = lc.currency(invoice_total, grouping=True)
 
     # display the results
-    print(f"Order total:        {order_total:10,}")
+    print(f"Order total:        {order_total:>10}")
     print(f"Discount amount:    {discount:10,}")
     print(f"Subtotal:           {subtotal:10,}")
+    print(f"Shipping Costs:     {shipping_cost:10,}")
     print(f"Sales tax:          {sales_tax:10,}")
-    print(f"Invoice total:      {invoice_total:10,}")
+    print(f"Invoice total:      {invoice_total:>10}")
     print()
 
     choice = input("Continue? (y/n): ")    
